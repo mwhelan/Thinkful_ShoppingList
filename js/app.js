04f8512cfd22fc9279a18a3e6cf10a23ac8d5cb7
Michael Whelan <mjmwdev@gmail.com>
2014-05-05 14:14:14 +0100

    basic add, remove, complete list functionality.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,50 @@
 $(document).ready(function() {
-    $("#Add").click(addItem);
+	/* add item on button click */
+	$("#AddButton").click(addItem);
+
+	/* add item when Enter key pressed when cursor is in textbox */
+	$("#newItemTextBox").keydown(function(event) {
+		var keycode = event.keyCode ? event.keyCode : event.which;
+		if(keycode == 13){
+			addItem();
+		}
+	});
+
+	/* remove item from list */
+	$("#list").on("click", "a", function() {
+		var listItem = $(this).closest("li");
+		var itemName = listItem.find("span").text();
+		var message = "Are you sure you want to remove '" + itemName + "' from the list?";
+		if(window.confirm(message)) {
+			listItem.remove();
+		}
+	});
+
+	/* strikethrough text if checkbox selected */
+	$("#list").on("change", "input:checkbox",function () {
+		var item = $(this).closest("li").find("span");
+		if($(this).is(":checked")) {
+			item.addClass("completed");
+		}
+		else
+			item.removeClass("completed"); 
+	});
 });
 
 function addItem() {
-  alert("addItem");
+	var newItem = $("#newItemTextBox").val();
+	if(newItem.length === 0) {
+		alert("You must enter an item to be added.");
+	}
+
+	var listItem = createListItem(newItem);
+	$("#list").append(listItem);
+	$("#newItemTextBox").val("");
+}
+
+function createListItem(newItem) {
+	var listItem = "<li><input type='checkbox'>"; 
+	listItem += "<span>" + newItem + "</span>";
+	listItem += "<a href='#'>Remove</a></li>";
+	return listItem; 
 }
